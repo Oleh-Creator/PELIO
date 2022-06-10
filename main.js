a5b83786ea2cb3daba52170da863e78df888b6d6
Oleh <105147109+Oleh-Creator@users.noreply.github.com>
2022-06-10 22:13:38 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,237 @@
+
+
+let click = 0
+login.onclick=function(){
+
+    if (click === 0) {
+        Card2.style.display = 'flex';
+        login.style.display = 'none';
+          click = 1;
+      } else if (click === 1) {
+        Card2.style.display = 'none';
+          click = 0;
+      }
+  
+  } 
+  ExitCard.onclick=function(){
+    Card2.style.display = 'none';
+    login.style.display = 'block';
+  }
+  butCard.onclick=function(){
+    Card2.style.display = 'none';
+    login.style.display = 'block';
+  }
+
+GreenOval.onmousemove=function(){
+    GreenOval.style.left='205px'
+}
+GreenOval.onmouseout=function(){
+    GreenOval.style.left='190px'
+}
+BlueOval.onmousemove=function(){
+    BlueOval.style.left='205px'
+}
+BlueOval.onmouseout=function(){
+    BlueOval.style.left='190px'
+}
+
+YellowOval.onmousemove=function(){
+    YellowOval.style.left='205px'
+}
+YellowOval.onmouseout=function(){
+    YellowOval.style.left='190px'
+}
+
+RedOval.onmousemove=function(){
+    RedOval.style.left='245px'
+}
+RedOval.onmouseout=function(){
+    RedOval.style.left='230px'
+}
+  button1.onclick=function(){
+    button1.style.background='#0E6EFF'
+    Dashboard.style.color='white'
+    button2.style.background='white'
+    button2.style.color='black'
+    button3.style.background='white'
+    button3.style.color='black'
+    button4.style.background='white'
+    button4.style.color='black'
+    button5.style.background='white'
+    button5.style.color='black'
+    button6.style.background='white'
+    button6.style.color='black'
+  }
+  button2.onclick=function(){
+    button2.style.background='#0E6EFF'
+    button2.style.color='white'
+    button1.style.background='white'
+    Dashboard.style.color='black'
+    button3.style.background='white'
+    button3.style.color='black'
+    button4.style.background='white'
+    button4.style.color='black'
+    button5.style.background='white'
+    button5.style.color='black'
+    button6.style.background='white'
+    button6.style.color='black'
+  }
+  button3.onclick=function(){
+    button3.style.background='#0E6EFF'
+    button3.style.color='white'
+    button1.style.background='white'
+    Dashboard.style.color='black'
+    button2.style.background='white'
+    button2.style.color='black'
+    button4.style.background='white'
+    button4.style.color='black'
+    button5.style.background='white'
+    button5.style.color='black'
+    button6.style.background='white'
+    button6.style.color='black'
+  }
+  button4.onclick=function(){
+    button4.style.background='#0E6EFF'
+    button4.style.color='white'
+    button1.style.background='white'
+    Dashboard.style.color='black'
+    button2.style.background='white'
+    button2.style.color='black'
+    button3.style.background='white'
+    button3.style.color='black'
+    button5.style.background='white'
+    button5.style.color='black'
+    button6.style.background='white'
+    button6.style.color='black'
+  }
+  button5.onclick=function(){
+    button5.style.background='#0E6EFF'
+    button5.style.color='white'
+    button1.style.background='white'
+    Dashboard.style.color='black'
+    button2.style.background='white'
+    button2.style.color='black'
+    button3.style.background='white'
+    button3.style.color='black'
+    button4.style.background='white'
+    button4.style.color='black'
+    button6.style.background='white'
+    button6.style.color='black'
+  }
+  button6.onclick=function(){
+    button6.style.background='#0E6EFF'
+    button6.style.color='white'
+    button1.style.background='white'
+    Dashboard.style.color='black'
+    button2.style.background='white'
+    button2.style.color='black'
+    button3.style.background='white'
+    button3.style.color='black'
+    button4.style.background='white'
+    button4.style.color='black'
+    button5.style.background='white'
+    button5.style.color='black'
+  }
+  slideBox1.onclick=function(){
+    slideBox1.style.background='#0E6EFF' 
+    slideBox1.style.color='white' 
+    slideBox2.style.background='white' 
+    slideBox2.style.color='black' 
+    slideBox3.style.background='white' 
+    slideBox3.style.color='black' 
+    slideBox4.style.background='white' 
+    slideBox4.style.color='black'  
+    slideBox5.style.background='white'  
+    slideBox5.style.color='black' 
+  }
+  slideBox2.onclick=function(){
+    slideBox2.style.background='#0E6EFF' 
+    slideBox2.style.color='white' 
+    slideBox1.style.background='white' 
+    slideBox1.style.color='black' 
+    slideBox3.style.background='white' 
+    slideBox3.style.color='black' 
+    slideBox4.style.background='white'  
+    slideBox4.style.color='black' 
+    slideBox5.style.background='white' 
+    slideBox5.style.color='black' 
+  }
+  slideBox3.onclick=function(){
+    slideBox3.style.background='#0E6EFF' 
+    slideBox3.style.color='white' 
+    slideBox1.style.background='white' 
+    slideBox1.style.color='black' 
+    slideBox2.style.background='white' 
+    slideBox2.style.color='black' 
+    slideBox4.style.background='white' 
+    slideBox4.style.color='black' 
+    slideBox5.style.background='white' 
+    slideBox5.style.color='black'   
+  }
+  slideBox4.onclick=function(){
+    slideBox4.style.background='#0E6EFF'
+    slideBox4.style.color='white'  
+    slideBox1.style.background='white' 
+    slideBox1.style.color='black' 
+    slideBox2.style.background='white' 
+    slideBox2.style.color='black' 
+    slideBox3.style.background='white' 
+    slideBox3.style.color='black' 
+    slideBox5.style.background='white'
+    slideBox5.style.color='black'   
+  }
+  slideBox5.onclick=function(){
+    slideBox5.style.background='#0E6EFF' 
+    slideBox1.style.background='white'
+    slideBox1.style.color='black'  
+    slideBox2.style.background='white'
+    slideBox2.style.color='black'  
+    slideBox4.style.background='white' 
+    slideBox4.style.color='black' 
+    slideBox3.style.background='white'   
+    slideBox3.style.color='black' 
+  }
+  priseBox.onclick=function(){
+    priseBox.style.background='#0E6EFF'
+    PriseBoxText.style.color='white'
+    Prise.style.color='white'
+    Taxes.style.background='#F5F5F5'
+    Taxes.style.color='black'
+    Expenses.style.background='#F5F5F5'
+    Expenses.style.color='black'
+    Debt.style.background='#F5F5F5'
+    Debt.style.color='black'
+  }
+  Taxes.onclick=function(){
+    Taxes.style.background='#0E6EFF'
+    Taxes.style.color='white'
+    priseBox.style.background='#F5F5F5'
+    PriseBoxText.style.color='black'
+    Prise.style.color='black'
+    Expenses.style.background='#F5F5F5'
+    Expenses.style.color='black'
+    Debt.style.background='#F5F5F5'
+    Debt.style.color='black'
+  }
+  Expenses.onclick=function(){
+    Expenses.style.background='#0E6EFF'
+    Expenses.style.color='white'
+    Taxes.style.background='#F5F5F5'
+    Taxes.style.color='black'
+    priseBox.style.background='#F5F5F5'
+    PriseBoxText.style.color='black'
+    Prise.style.color='black'
+    Debt.style.background='#F5F5F5'
+    Debt.style.color='black'
+  }
+  Debt.onclick=function(){
+    Debt.style.background='#0E6EFF'
+    Debt.style.color='white'
+    Taxes.style.background='#F5F5F5'
+    Taxes.style.color='black'
+    priseBox.style.background='#F5F5F5'
+    PriseBoxText.style.color='black'
+    Prise.style.color='black'
+    Expenses.style.background='#F5F5F5'
+    Expenses.style.color='black'
+  }
